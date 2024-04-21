36357bfc932533d8a76aa5adcb83cf6d1393f7df
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-04-21 23:06:52 +0530

    added meeting

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,22 +18,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      layout: {
-        logoImageUrl: '/icons/yoom-logo.svg',
-        socialButtonsVariant:'iconButton'
-      },
-      variables: {
-        colorText: "#fff",
-        colorPrimary: "#0E78F9",
-        colorBackground: "#1c1f2e",
-        colorInputBackground: "#252a41",
-        colorInputText:"#fff"
-      }
-    }} >
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: "/icons/yoom-logo.svg",
+          socialButtonsVariant: "iconButton",
+        },
+        variables: {
+          colorText: "#fff",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#1c1f2e",
+          colorInputBackground: "#252a41",
+          colorInputText: "#fff",
+        },
+      }}
+    >
       <html lang="en">
-        <body className={`${inter.className} bg-dark-2  `}>{children}
-        <Toaster /></body>
+        <body className={`${inter.className} bg-dark-2  `}>
+          {children}
+          <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   );
